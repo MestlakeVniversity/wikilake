d1148f48d68f7e02c60b0f248ad72666c557edcc
WorldHim <worldhim@hotmail.com>
2026-09-03 15:03:50 +0800

    fix: 修正了错误的编辑分支

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/WorldHim/wikilake/tree/main/',
+            'https://github.com/WorldHim/wikilake/tree/master/',
           showLastUpdateTime: true,
         },
         blog: false,
